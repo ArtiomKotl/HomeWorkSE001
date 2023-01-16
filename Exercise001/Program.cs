b70b5a01baa4d56b23e1d8f4c84d51147b5ac675
artketler <artketler.ak@gmail.com>
2023-01-16 23:16:38 +0300

    Exercise 001 project

---
+++ Exercise001/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Нахождение максимального и минимального значения из двух чисел");
+
+Console.Write("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = b;
+
+if (b > max)  
+{
+  max = b;  min = a;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
+
